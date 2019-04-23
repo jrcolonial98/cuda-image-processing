1722f01b9e77b367415bf3d7d85cb8247b03c6df
Brendan Gregory <brendangregory@Brendans-MacBook-Pro.local>
2019-04-22 20:52:02 -0400

    checkpoint

--- main.cu
+++ main.cu
@@ -4,5 +4,12 @@
 int main() {
 	bmp image;
 	init_bmp(&image, "MARBLES.BMP");
+
+	/*
+	formatted_data = convert(image.data);
+	new_data = blur(formatted_data);
+	image.data = new_data;
+	*/
+
 	bmp_to_file(&image, "outtest.bmp");
 }
